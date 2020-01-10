8904cf519a6c19e1600e573405465524bcf83bc8
russian_rave <semisotnovroman2018@yandex.ru>
2020-01-10 15:25:09 +0400

    commit

--- src/main/java/myPackage/MessageHandlerController.java
+++ src/main/java/myPackage/MessageHandlerController.java
@@ -1,15 +1,13 @@
 package myPackage;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class MessageHandlerController {
 
     @PostMapping
-    public String execute() {
+    public String execute(@RequestParam(name = "type") String type) {
         return "ok";
     }
 
