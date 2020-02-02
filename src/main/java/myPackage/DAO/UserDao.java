4cc181f3320884dcc593e126456221c91c9d9462
russian_rave <semisotnovroman2018@yandex.ru>
2020-02-02 17:15:54 +0300

    commit

--- src/main/java/myPackage/DAO/UserDao.java
+++ src/main/java/myPackage/DAO/UserDao.java
@@ -14,23 +14,14 @@ public class UserDao extends AbstractDAO<User> {
 
     public User findOrCreateByVkId(int vkId) {
         Session session = sessionFactory.openSession();
-        Query query = session.createQuery("select user from User user where user.vkId = :vk_id");
+        Query<User> query = session.createQuery("select user from User user where user.vkId = : vk_id");
         query.setParameter("vk_id", vkId);
 
-        User user = (User) query.getSingleResult();
-
-        if (user == null) {
+        return query.stream().findFirst().orElseGet(() -> {
             User newUser = new User(vkId);
             session.save(newUser);
             return newUser;
-        } else {
-            return user;
-        }
-     /*   return query.stream().findFirst().orElseGet(() -> {
-            User newUser = new User(vkId);
-            session.save(newUser);
-            return newUser;
-        });*/
+        });
     }
 
 }
